9f2eb4700ee2b6fc4876fdaf7d808d76f8168388
Andrew Walters <blazersand2000@users.noreply.github.com>
2020-09-26 15:21:23 -0400

    Delete environment.prod.ts

--- AngularApp/Finance/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  firebaseAPIKey: "AIzaSyAODw5cU-vU0T6yNMvOD-Wpl2sO2MIvJLc",
-  apiUrl: "https://financeapi20200924063525.azurewebsites.net/api/"
-};
